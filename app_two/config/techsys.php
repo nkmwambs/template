46872d5bf1c089678d514e20d3956c927a0e6d69
unknown <NKarisa@KEPIL136160121.CI.ORG>
2017-03-29 00:38:25 +0300

    Codeigniter Multiple Installation

---
+++ app_two/config/techsys.php
@@ -0,0 +1,10 @@
+<?php
+// Create customized config variables
+$config['techsys_address']= 'https://www.techsys.com/';
+
+$config['techsys_title']= 'Techsys Inc';
+
+$config['application_category'] = 'Grant Management System';
+
+//$config['curl_finance'] = 'http://localhost:8080/curl/index.php';
+$config['curl_finance'] = 'http://www.emackenya.org/sync/index.php';
